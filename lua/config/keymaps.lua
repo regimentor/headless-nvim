6bc675ef69bb65a8f0d48a9f411e7aa91e99b0df
Evgeny Shchekoldin <regimentor@yandex.ru>
2025-07-09 05:01:15 +0500

    feat: add some plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,11 +1,23 @@
 -- Open parrent directory in Oil
 vim.keymap.set("n", "<Leader>o", "<cmd>Oil --float<CR>", {desc = "Open parrent directory in Oil"});
--- Clear highlights on search when pressing <Esc> in normal mode
---  See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+vim.keymap.set('n', '<leader>w',"<cmd>w<CR>" , { desc = 'Save' })
+vim.keymap.set('n', '<leader>q',"<cmd>q<CR>" , { desc = 'Quit' })
+
+-- Search keymaps
+vim.keymap.set('n', '<leader>ff', function()
+  require('fzf-lua').files()
+end, {desc = "files search"})
+vim.keymap.set('n', '<leader>fg', function()
+  require('fzf-lua').live_grep()
+end, {desc = "grep search"})
+vim.keymap.set('n', '<leader>fd', function()
+  require('fzf-lua').builtin()
+end, {desc = "fzf all"})
+vim.keymap.set('n', '<leader><leader>', function()
+  require('fzf-lua').buffers()
+end, {desc = "buffers"})
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -14,3 +26,6 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -8,4 +8,13 @@ return {
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
+
+  config = function()
+    require('oil').setup({
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+      }
+    });
+  end
 }
