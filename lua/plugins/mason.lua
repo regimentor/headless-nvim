354003af2be5f047e614fc5b8514863926d32bd1
Evgeny Shchekoldin <regimentor@yandex.ru>
2025-07-15 22:22:47 +0500

    feat: Add Mason plugin configuration

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,30 +1,54 @@
 return {
     "mason-org/mason.nvim",
     opts = {},
-    dependencies = { 'mason-org/mason-lspconfig.nvim', 'neovim/nvim-lspconfig',
-        'WhoIsSethDaniel/mason-tool-installer.nvim' },
+    dependencies = {
+        "mason-org/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
     config = function()
-        local mason = require('mason')
-        local mason_lspconfig = require('mason-lspconfig')
-        local mason_tool_installer = require('mason-tool-installer')
+        local mason = require("mason")
+        local mason_lspconfig = require("mason-lspconfig")
+        local mason_tool_installer = require("mason-tool-installer")
 
         mason.setup()
         mason_lspconfig.setup({
             automatic_installation = true,
-            ensure_installed = { "lua_ls", "rust_analyzer", "gopls", "tsserver", "bashls", "jsonls", "yamlls", "html", "cssls" },
+            ensure_installed = {
+                "lua_ls",
+                "rust_analyzer",
+                "gopls",
+                "ts_ls",
+                "bashls",
+                "jsonls",
+                "yamlls",
+                "html",
+                "cssls",
+            },
             automatic_enable = {
                 exclude = {
                     "rust_analyzer",
                     "gopls",
-                    "lua_ls"
-                }
-            }
+                    "lua_ls",
+                },
+            },
         })
 
         mason_tool_installer.setup({
             automatic_installation = true,
-            ensure_installed = { "golangci-lint", "bash-language-server", "prettier", "stylua", "rustfmt", 'gofumpt',
-                'golines', 'gomodifytags', 'gotests', "isort", "black" }
+            ensure_installed = {
+                "golangci-lint",
+                "bash-language-server",
+                "prettier",
+                "stylua",
+                "rustfmt",
+                "gofumpt",
+                "golines",
+                "gomodifytags",
+                "gotests",
+                "isort",
+                "black",
+            },
         })
-    end
+    end,
 }
